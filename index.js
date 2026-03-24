6504ac63eab496584c242d03429a8cefaf5016e1
Smit <sp3896508@gmail.com>
2026-03-24 23:04:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import UserRoutes from './routes/userRoutes.js'
+import connectDB from './utils/db.js'
+
+
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(express.json())
+
+app.use('api/v1/users',UserRoutes)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
